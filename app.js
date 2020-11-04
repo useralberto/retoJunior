111bc44592dfc4f9d9517c2e4b540616c8b9a18b
useralberto <alonso.cruz2795@gmail.com>
2020-11-03 22:00:52 -0600

    Se agrego animacion en el porcentaje

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-let bar = document.querySelector(".content2__Bar--comple");
-setTimeout(
-  function(){
-    bar.style.width = "70%";
+let i = 0;
+(function(){
+  if (i == 0) {
+    i = 1;
+    let bar = document.querySelector(".content2__Bar--comple");
+    let porce = document.querySelector("#porcentaje");
+    let width = 10;
+    let pporcent = 0;
+    let id = setInterval(function(){
+      if (width >= 70) {
+        clearInterval(id);
+        porce.innerHTML = pporcent+5;
+        i = 0;
+      } else {
+        width++;
+        pporcent+=3;
+        bar.style.width = width + "%";
+        porce.innerHTML = pporcent;
+      }
+    }, 100);
   }
-, 100);
+})();
